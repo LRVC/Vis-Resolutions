5a6889d16b0f6394cc8a4992200ece306237d597
Reece <lrv378@gmail.com>
2015-08-09 20:00:31 -0600

    Add dependencies

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-var http = require('http');
+var http = require('http')
+	, express = require('express')
+	, app = express()
+	, server = app.listen(process.env.PORT || 3000);
+
+app.use(express.static(path.join(__dirname, './www')));
