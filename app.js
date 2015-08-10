9264cdb8795c79f72691560a360273a18ecaabea
Reece <lrv378@gmail.com>
2015-08-09 20:16:11 -0600

    Add Skeleton CSS boilerplate

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var http = require('http')
 	, express = require('express')
 	, app = express()
+	, path = require('path')
 	, server = app.listen(process.env.PORT || 3000);
 
-app.use(express.static(path.join(__dirname, './www')));
+app.use(express.static(path.join(__dirname, '/www')));
